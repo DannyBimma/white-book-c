cf92861898504ab2e916883b9203ee82742ae55b
DannyBimma <dantrotman92@gmail.com>
2025-06-01 20:20:29 -0400

    Add newline chars.

--- get_char.c
+++ get_char.c
@@ -5,11 +5,11 @@ int main(void)
     // get_char value
     if ((getchar()) != EOF == 0)
     {
-        printf("It's Zero-chan!!");
+        printf("It's Zero-chan!!\n");
     }
     else if ((getchar()) != EOF == 1)
     {
-        printf("It's One-chan!!");
+        printf("It's One-chan!!\n");
     }
     else
     {
